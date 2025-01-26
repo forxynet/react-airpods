292790aca2d85257caa4b6e721af257612324044
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-27 02:25:43 +0300

    updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,58 @@
-import React from "react";
-
+import { useState, useEffect } from 'react';
+import Navbar from './Navbar'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaMessage } from 'react-icons/fa6';
+import { HeadphoneData } from '../data/MockData';
 const Hero = () => {
-  return <div>Hero</div>;
+  const [activeData, setActiveData] = useState(HeadphoneData[0]);
+  const [currentIndext, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % HeadphoneData.length);
+    }, 3000) // change every 3 seconds
+
+    return () => clearInterval(interval); // cleanup ,nterval on component unmount
+  }, [currentIndext])
+
+  useEffect(() => {
+    setActiveData(HeadphoneData[currentIndext]);
+  }, [currentIndext])
+
+  return (
+    <section className='bg-red-400 text-white'>
+      <Navbar />
+      <div className="container grid grid-cols-1 md:grid-cols-2 h-screen md:h-[700px] relative">
+        {/* headphoen info section */}
+        <div className='flex flex-col justify-center py-14 md:py-0 xl:max-w-[500px] order-2 md:order-1'>
+          <div className='space-y-5md:space-y-7 text-center md:text-left'>
+            <h1 className='text-3xl lg:text-4xl xl:text-5xl font-bold'>{activeData.title}</h1>
+            <p className='text-sm leading-loose text-white/80'>{activeData.subtitle}</p>
+            <p className='text-3xl lg:text-4xl xl:text-5xl font-bold'>{activeData.price}</p>
+            {/* social icons section */}
+            <div className='flex items-center justify-center md:justify-center gap-4 text-3xl'>
+              <FaInstagram className='cursor-pointer border bordered-full p-[6px]' />
+              <FaFacebook className='cursor-pointer border bordered-full p-[6px]' />
+              <FaTwitter className='cursor-pointer border bordered-full p-[6px]' />
+            </div>
+          </div>
+        </div>
+        {/* headphoen img section */}
+        <div className='flex flex-col items-center justify-center order-1 md:order-2 relative'>
+          <img
+            src={activeData.image}
+            alt=""
+            className='w-[300px] md:w-[400px] xl:w-[500px] z-10'
+          />
+          <div className='text-[300px] absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 text-white/5 font-poppins font-extrabold'>{activeData.modal}</div>
+        </div>
+        {/* chat icon section */}
+        <div className='absolute bottom-10 right-10 z-[999]'>
+          <FaMessage className='text-2xl cursor-pointer' />
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { MdMenu } from 'react-icons/md';
+import { FaApple, FaRegUser } from 'react-icons/fa'
+import { NavbarData } from '../data/MockData';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
-};
 
+  return (
+    <nav className="text-white py-5">
+      <div className="container flex justify-between items-center">
+        {/* logo section */}
+        <div className='flex items-center gap-2 text-3xl font-semibold'>
+          <FaApple />
+          AirPods Maz
+        </div>
+        {/* menu section */}
+        <div className='hidden md:block'>
+          <ul className='flex items-center gap-4'>
+            {NavbarData.map(item => {
+              return (
+                <li key={item.id}>
+                  <a
+                    href={item.link}
+                    className='inline-block text-base py-2 px-3 uppercase'
+                  >
+                    {item.title}
+                  </a>
+                </li>
+              )
+            })}
+            <button className='text-xl ps-14'>
+              <FaRegUser />
+            </button>
+          </ul>
+        </div>
+        {/* hamburger menu section */}
+        <div className='md:hidden'>
+          <MdMenu className='text-4xl' />
+        </div>
+      </div>
+    </nav>
+  )
+}
 export default Navbar;
